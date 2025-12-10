21c542c840b8a3408621486bcc1ebd050d1b37f8
Fadhli170402 <fadliadliana5@gmail.com>
2025-12-10 14:14:28 +0700

    Geolocation Permission

--- app.js
+++ app.js
@@ -1,7 +1,24 @@
+function success(position){
+    console.log(position);
+}
+function error(){
+    console.log('Error: Geolocation is not available.');
+}
+
+function userLocation(){
+    if(!navigator.geolocation){
+        alert('Geolocation is not supported by this browser.');
+    }else{
+        navigator.geolocation.getCurrentPosition(success, error);
+    }
+}
+
 function index(){
     let app = document.getElementById('app');
     let h3 = document.createElement('h3');
     h3.innerHTML = 'Prayer Time';
     app.appendChild(h3);
+
+    userLocation();
 }
 index();
